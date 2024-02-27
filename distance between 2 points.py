78f7ee8a3bc591a0ffafe5a39e17f970ae969f67
Kavishma S <155907940+Kavishma@users.noreply.github.com>
2024-02-27 20:08:17 +0530

    Add files via upload

---
+++ distance between 2 points.py
@@ -0,0 +1,10 @@
+#distance b/w 2 points
+
+import math
+
+x1=int(input("enter value:"))
+x2=int(input("enter value:"))
+y1=int(input("enter value:"))
+y2=int(input("enter value:"))
+distance=math.sqrt(((x2-x1)**2)+((y2-y1)**2))
+print("distance b/w 2 points:",distance)
